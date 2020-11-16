4a90485f2eaebc41cdf68ccae7b8610c1fc49ddb
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-15 22:14:58 -0500

    Refactorizacion estilos globales, logo photocard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
-
+import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () =>(
     <div>
-
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
+        <ListOfPhotoCards />
     </div>
 )
 
