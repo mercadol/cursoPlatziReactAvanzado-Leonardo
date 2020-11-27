53421c9a8ab82aaba51a4f638979fccfc7a549dc
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-27 15:49:28 -0500

    Implemento Graphql en ListOfPhotoCards

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { graphql } from 'graphql'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
 
-export const ListOfPhotoCards = () =>{
+/*
+const withPhotos = graphql(gql`query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}`)
+const ListOfPhotoCardsComponent = ({data: {photos = []}}={}) =>{
     return(
         <ul>
-            {[1,2,3,4,5,6,7,8].map(id=> <PhotoCard key={id} id={id}
+            {photos.map(photo=> <PhotoCard key={photo.id} {...photo}
             />)}
         </ul>
     )
 }
+
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
+*/
+export const ListOfPhotoCards=()=>{
+  return <Query query={gql`{
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }`}>
+    {({loading, error, data})=>{
+      if(loading) return <p>Loading...</p>
+      if(error) return <p>Error!...</p>
+      return(<ul>{data.photos.map(photo=>{
+        return <PhotoCard key={photo.id} {...photo}/>
+      })}</ul>)
+    }}
+  </Query>
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
 import {App} from './App'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+const client = new ApolloClient({
+    uri: 'https://react-avanzado-leonardo.vercel.app/graphql'
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+, document.getElementById('app'))
