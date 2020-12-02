f6d4a22c9ea65f492ed23d8b20a4db47868ae30e
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-02 12:46:32 -0500

    ReachRouter y uso de link, se agregó pages/home

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -42,7 +42,7 @@ export const ListOfCategories = () => {
                 loading
                 ? <Item key='loading'><Category /></Item>
                 : categories.map(category => <Item key={category.id}>               
-                <Category {...category} />
+                <Category {...category} path={`/pet/${category.id}`} />
                 </Item>)
             }
         </List>
--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -1,31 +1,21 @@
 import React from 'react'
 import { PhotoCard } from '../components/PhotoCard'
-import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
+import { GET_SINGLE_PHOTO } from '../hoc/singlePhoto'
 
-const query = gql`
-query getSinglePhoto($id:ID!){
-    photo(id:$id){
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`
+
+
+const renderProp = ({ loading, error, data }) => {
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!...</p>
+
+  const { photo = {} } = data
+  return <PhotoCard {...photo} />
+}
 
 export const PhotoCardWithQuery = ({ id }) => (
-  <Query query={query} variables={{ id }}>
-  {
-    ({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>
-      if (error) return <p>Error!...</p>
-      const { photo = {} } = data
-      return <PhotoCard {...photo} />
-    }
-  }
+  <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+  {renderProp}
   </Query>
 )
 
