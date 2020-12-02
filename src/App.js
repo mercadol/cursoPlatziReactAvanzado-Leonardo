68096efdcb7eb40c2f35927de65c1491be41282b
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-02 14:10:50 -0500

    pagina detail y detalles de enrutamiento

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
+import { from } from 'apollo-boost'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-  console.log(detailId)
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:categoryId' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </div>
   )
 }
