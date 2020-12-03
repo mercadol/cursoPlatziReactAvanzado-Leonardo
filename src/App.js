2328b3a35402378ddc10b782fedc2f4482ab6def
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-02 19:29:15 -0500

    Rutas y paginas protegidas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
-import { from } from 'apollo-boost'
+import { NavBar } from './components/NavBar'
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 export const App = () => {
   return (
     <div>
@@ -16,7 +21,23 @@ export const App = () => {
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        
       </Router>
+      <UserLogged>
+      {
+        ({isAuth}) =>
+        isAuth
+          ? <Router>
+            <Favs path='/favs' />
+            <User path='/user' />
+          </Router>
+          :<Router>
+            <NotRegisteredUser path='/favs' />
+            <NotRegisteredUser path='/user' />
+          </Router>
+      }
+      </UserLogged>
+      <NavBar />
     </div>
   )
 }
