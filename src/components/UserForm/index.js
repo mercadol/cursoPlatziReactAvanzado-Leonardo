2532819073c0ba36f24929773839d52866434cc3
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-23 17:19:13 -0500

    ultimos cambios diciembre 2020

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,25 +1,39 @@
 /* eslint-disable react/jsx-handler-names */
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
+import { Button, Error, Form, Input, Title } from './styles'
 
-export const UserForm = ({ onSubmint }) => {
+export const UserForm = ({ error, diseabled, onSubmint, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmint({
+      email: email.value,
+      password: password.value
+    })
+  }
   return (
-    <form onSubmint={onSubmint}>
-      <input
-        placeholder='Email'
-        value={email.value}
-        onChange={email.onChange}
-      />
-      <input
-        placeholder='Passord'
-        type='password'
-        value={password.value}
-        onChange={password.onChange}
-      />
-      <button>Iniciar Sesión</button>
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form diseabled={diseabled} onSubmint={handleSubmit}>
+        <Input
+          diseabled={diseabled}
+          placeholder='Email'
+          value={email.value}
+          onChange={email.onChange}
+        />
+        <Input
+          diseabled={diseabled}
+          placeholder='Passord'
+          type='password'
+          value={password.value}
+          onChange={password.onChange}
+        />
+        <Button diseabled={diseabled}>{title}</Button>
+      </Form>
+      {error && <Error>{error}</Error>}
+    </>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,11 +1,47 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return <UserForm onSubmint={activateAuth} />
-    }}
-  </Context.Consumer>
-)
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+  return (
+    <>
+      <RegisterMutation>
+        {
+          (register, { data = {}, loading, error }) => {
+            const handleSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              register({ variables }).then(({ data }) => {
+                console.log(data)
+                const { signup } = data
+                activateAuth(signup)
+              })
+            }
+            const errorMsg = error && 'El usuario ya existe o hay algún problema'
+            return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmint={handleSubmit} />
+          }
+        }
+      </RegisterMutation>
+      <LoginMutation>
+        {
+          (login, { data = {}, loading, error }) => {
+            const handleSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              login({ variables }).then(({ data }) => {
+                console.log(data)
+                const { login } = data
+                activateAuth(login)
+              })
+            }
+            const errorMsg = error && 'El usuario no existe o la contraseña es incorrecta'
+            return <UserForm disabled={loading} error={errorMsg} title='Inicio de seción' onSubmint={handleSubmit} />
+          }
+        }
+      </LoginMutation>
+    </>
+  )
+}
